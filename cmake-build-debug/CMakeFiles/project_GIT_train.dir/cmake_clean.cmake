016f0ffd8903ff1cca75bcadd726d3b5055c1e69
235847 <235847@edu.p.lodz.pl>
2021-02-05 16:10:44 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/project_GIT_train.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_GIT_train.dir/main.cpp.obj"
+  "project_GIT_train.exe"
+  "project_GIT_train.exe.manifest"
+  "project_GIT_train.lib"
+  "project_GIT_train.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_GIT_train.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
